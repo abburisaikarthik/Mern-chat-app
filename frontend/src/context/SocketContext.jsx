b6152efaf37a70d143710407dbff5c274847ab8e
karthik2802 <83510357+abburisaikarthik@users.noreply.github.com>
2024-03-13 19:39:50 +0530

    Socket Url Updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -14,7 +14,7 @@ export const SocketContextProvider = ({ children }) => {
   const { authUser } = useAuthContext();
   useEffect(() => {
     if (authUser) {
-      const newSocket = io("http://localhost:5000", {
+      const newSocket = io("https://chat-app-zfwh.onrender.com", {
         query: {
           userId: authUser._id,
         },
