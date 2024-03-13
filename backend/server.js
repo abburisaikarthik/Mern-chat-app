a8d9b214e77522278de7fe6dd18d0a81cc584f53
karthik2802 <83510357+abburisaikarthik@users.noreply.github.com>
2024-03-13 18:19:10 +0530

    Added socket-io

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,8 @@ import messageRoutes from "./Routes/message.routes.js";
 import userRoutes from "./Routes/user.routes.js";
 
 import connectTOMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
@@ -24,7 +24,7 @@ app.use("/api/users", userRoutes);
 //   res.send("Hello World!");
 // });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectTOMongoDB();
   console.log(`Server running on port ${PORT}`);
 });
